b0ec6a8ea4fab6120fa3e39df67405d6872b7870
wamas <wamas@LUNANIA>
2023-04-21 11:03:31 +0200

    super secret class

---
+++ TestGithub/SuperSecretClassNameDontWorryAboutItLikeYoureNotGoingToGuessThisAnyways.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGithub
+{
+    public class SuperSecretClassNameDontWorryAboutItLikeYoureNotGoingToGuessThisAnyways
+    {
+        public string SecretVariable1 { get; set; }
+        public string SecretVariable2 { get; set; }
+        public double SuperSecretVariable1 { get; set; }
+        public char UltraSecretVariable { get; set; }
+
+        public SuperSecretClassNameDontWorryAboutItLikeYoureNotGoingToGuessThisAnyways(string secretVariable1, string secretVariable2, double superSecretVariable1, char ultraSecretVariable)
+        {
+            SecretVariable1 = secretVariable1;
+            SecretVariable2 = secretVariable2;
+            SuperSecretVariable1 = superSecretVariable1;
+            UltraSecretVariable = ultraSecretVariable;
+        }
+    }
+}
