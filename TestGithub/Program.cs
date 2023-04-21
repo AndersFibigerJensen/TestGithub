4d596f90c8ca38ca8e007020ade4fa8aedd9aa9d
Adam Nørrind Schaumburg-Müller <adam8154@edu.zealand.dk>
2023-04-21 11:12:12 +0200

    a

--- TestGithub/Program.cs
+++ TestGithub/Program.cs
@@ -1,3 +1,8 @@
 ﻿
 // See https://aka.ms/new-console-template for more information
+using TestGithub;
+
 Console.WriteLine("Hello, World!");
+
+Class123 c123 = new Class123("Class123", "Epic");
+Console.WriteLine(c123.Name);
