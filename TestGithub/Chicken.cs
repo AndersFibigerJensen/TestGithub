65382fd962cfa58db904b5980fc6d2e4a5a89f68
Adam Nørrind Schaumburg-Müller <adam8154@edu.zealand.dk>
2023-04-21 12:40:05 +0200

    chicken

---
+++ TestGithub/Chicken.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGithub
+{
+    public class Chicken
+    {
+        public int NrOfEggs { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Chicken(int nrOfEggs, string name, int age)
+        {
+            NrOfEggs = nrOfEggs;
+            Name = name;
+            Age = age;
+        }
+    }
+}
